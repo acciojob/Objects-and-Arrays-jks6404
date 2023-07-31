a37fa1fc74321d21387c9da13a09025447596cbf
Janu Kumar Singh <119485859+jks6404@users.noreply.github.com>
2023-07-31 21:00:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ let team1 = [...players];
 let cap1 = {...person};
 //  make sure the name of variables is exactly same as mentioned in the problem statement
 
-console.log(players)
-console.log(team1)
-console.log(person)
-console.log(cap1)
-console.log(team)
+window.players = players;
+window.person = person;
+window.team = team;
+window.team1 = team1;
+window.cap1 = cap1;
