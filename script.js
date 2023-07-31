23a3a36c6c0db83d09518142f09722e8cd84930a
Janu Kumar Singh <119485859+jks6404@users.noreply.github.com>
2023-07-31 20:57:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const players = ["John", "Bob", "Alice", "Poppy"];
 
-const person = {
+const person = {   //this is a object
   name: "John Doe",
   age: 80,
 };
-let team = players;
-
+let team = players; //this is var
+//here spread operator has been used to copy variable team
 let team1 = [...players];
 
 let cap1 = {...person};
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+//  make sure the name of variables is exactly same as mentioned in the problem statement
 
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+console.log(players)
+console.log(team1)
+console.log(person)
+console.log(cap1)
+console.log(team)
